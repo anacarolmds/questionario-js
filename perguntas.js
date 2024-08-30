26b10d8387f21b81d0b443f3a1458030b13801c6
anacarolina# <117647684+anacarolmds@users.noreply.github.com>
2024-08-29 21:57:30 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o maior animal marinho existente?",
+        alternativas: [
+            {
+                texto: "A)Baleia Azul",
+                afirmacao: [
+                    "A)Baleia Azul"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Baleia Jubarte",
+                afirmacao: [
+                    "A) Baleia Azul"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual animal marinho é conhecido por mudar de cor para se camuflar?",
+        alternativas: [
+            {
+                texto: "a) Estrela-do-mar",
+                afirmacao: [
+                    "b) Polvo",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) Polvo",
+                afirmacao: [
+                    "b) Polvo"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a profundidade média dos oceanos?",
+        alternativas: [
+            {
+                texto: "A) 3.800 metros",
+                afirmacao: [
+                    "A) 3.800 metros"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "b) 2.500 metros",
+                afirmacao: [
+                    "A)  3.800 metros"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Os corais são?",
+        alternativas: [
+            {
+                texto: "a) Plantas",
+                afirmacao: [
+                    "b) Animais"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "b) Animais",
+                afirmacao: [
+                    "b) Animais"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual animal marinho é conhecido por sua inteligência e uso de ferramentas?",
+        alternativas: [
+            {
+                texto: "a) Peixe-palhaço",
+                afirmacao: [
+                    "b) Polvo"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "b) Polvo",
+                afirmacao: [
+                    "b) Polvo"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Que fenômeno faz com que o nível do mar suba e desça diariamente?",
+        alternativas: [
+            {
+                texto: "A)Gravidade da Lua",
+                afirmacao: [
+                    "A)Gravidade da Lua"
+                ],
+            },
+            {
+                texto: "B)Movimento das placas tectônicas",
+                afirmacao: [
+                    "A) Gravidade da Lua"
+                ],
+            },
+        ]
+    },
+];
